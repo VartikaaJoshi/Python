e0e54c4c6eba5e9e89e20ac7dda5896a15a914b8
VartikaaJoshi <96254851+VartikaaJoshi@users.noreply.github.com>
2022-10-17 11:40:15 -0400

    Create lifespan left.py

---
+++ Years/lifespan left.py
@@ -0,0 +1,20 @@
+
+age = input("What is your current age?")
+
+
+
+age1 = int(age)
+avg_lifespan = 85
+
+years_left = avg_lifespan - age1
+
+months_left = years_left * 12
+
+weeks_left = (years_left * 365) / 7
+
+years_left1 = str(years_left)
+months_left1 = str(months_left)
+weeks_left1 = str(weeks_left)
+
+print("You have " + years_left1 + "yrs, " + months_left1 + "months and " +
+      weeks_left1 + " weeks left.")
